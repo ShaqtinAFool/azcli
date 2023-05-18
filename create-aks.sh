9b833f267f31c51ec40e535a7b34a8d3c14c0a61
tony lee <tony.lee@auo.com>
2023-05-18 16:01:47 +0800

    update

--- create-aks.sh
+++ create-aks.sh
@@ -3,21 +3,18 @@
 export VNET_RESOURCE_ID="/subscriptions/56d91c85-cf61-42ee-82df-68714c92ab74/resourceGroups/Infra/providers/Microsoft.Network/virtualNetworks/SDZVNet"
 export AKS_RESOURCE_GROUP=Test
 export AKS_LOCATION=japaneast
-export AKS_GATEWAY=tonylee
-export CLUSTER_NAME=tonylee
+export AKS_GATEWAY=tonylee-asc
+export CLUSTER_NAME=tonylee-asc
 
 # AKS (CNI) + AppGw
 az aks create \
     --resource-group ${AKS_RESOURCE_GROUP} \
     --name ${CLUSTER_NAME} \
     --generate-ssh-keys \
-    --enable-addons ingress-appgw \
     --enable-private-cluster \
-    --dns-service-ip 10.0.0.10 \
-    --service-cidr 10.0.0.0/16 \
-    --docker-bridge-address 172.17.0.1/16 \
     --network-plugin azure \
     --vnet-subnet-id "${VNET_RESOURCE_ID}/subnets/AUO-ABS000_AKS" \
+    --enable-addons ingress-appgw \
     --appgw-name ${AKS_GATEWAY} \
     --appgw-subnet-id "${VNET_RESOURCE_ID}/Subnets/AUO-ABS000_AppGw" \
     --load-balancer-sku standard \
@@ -29,9 +26,9 @@ az aks create \
     --resource-group ${AKS_RESOURCE_GROUP} \
     --name ${CLUSTER_NAME} \
     --generate-ssh-keys \
-    --enable-addons ingress-appgw \
     --enable-private-cluster \
     --network-plugin kubenet \
+    --enable-addons ingress-appgw \
     --appgw-name ${AKS_GATEWAY} \
     --appgw-subnet-id "${VNET_RESOURCE_ID}/Subnets/AUO-ABS000_AppGw" \
     --load-balancer-sku standard \
