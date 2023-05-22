14a3ae86251877480a805a48ca6577bcc4a9d718
MBP18 <xu3ej04u454@gmail.com>
2023-05-22 22:24:01 +0800

    update

--- create-aks.sh
+++ create-aks.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 export VNET_RESOURCE_ID="/subscriptions/56d91c85-cf61-42ee-82df-68714c92ab74/resourceGroups/Infra/providers/Microsoft.Network/virtualNetworks/SDZVNet"
-export AKS_RESOURCE_GROUP=Test
+export RESOURCE_GROUP=Test
 export AKS_LOCATION=japaneast
-export AKS_GATEWAY=tonylee-asc
-export CLUSTER_NAME=tonylee-asc
+export APP_GATEWAY=tonylee-asc
+export AKS_CLUSTER_NAME=tonylee-asc
 
 # AKS (CNI) + AppGw
 az aks create \
-    --resource-group ${AKS_RESOURCE_GROUP} \
-    --name ${CLUSTER_NAME} \
+    --resource-group ${RESOURCE_GROUP} \
+    --name ${AKS_CLUSTER_NAME} \
     --generate-ssh-keys \
     --enable-private-cluster \
     --network-plugin azure \
     --vnet-subnet-id "${VNET_RESOURCE_ID}/subnets/AUO-ABS000_AKS" \
     --enable-addons ingress-appgw \
-    --appgw-name ${AKS_GATEWAY} \
+    --appgw-name ${APP_GATEWAY} \
     --appgw-subnet-id "${VNET_RESOURCE_ID}/Subnets/AUO-ABS000_AppGw" \
     --load-balancer-sku standard \
     --node-vm-size Standard_DS3_v2 \
@@ -23,14 +23,20 @@ az aks create \
 
 # AKS (kubernet) + AppGw
 az aks create \
-    --resource-group ${AKS_RESOURCE_GROUP} \
-    --name ${CLUSTER_NAME} \
+    --resource-group ${RESOURCE_GROUP} \
+    --name ${AKS_CLUSTER_NAME} \
     --generate-ssh-keys \
     --enable-private-cluster \
     --network-plugin kubenet \
     --enable-addons ingress-appgw \
-    --appgw-name ${AKS_GATEWAY} \
+    --appgw-name ${APP_GATEWAY} \
     --appgw-subnet-id "${VNET_RESOURCE_ID}/Subnets/AUO-ABS000_AppGw" \
     --load-balancer-sku standard \
     --node-vm-size Standard_DS3_v2 \
     --node-count 3
+
+# Connection Test
+az aks get-credentials \
+    --resource-group ${RESOURCE_GROUP} \
+    --name ${AKS_CLUSTER_NAME}
+kubectl get nodes
